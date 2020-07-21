8997a3035e8f46ffcc15c7bd7d73c3c1291a8b7c
Tom Jans <tjans@student.codam.nl>
2020-07-21 20:14:07 +0200

    Grafana config

--- srcs/grafana/image-setup.sh
+++ srcs/grafana/image-setup.sh
@@ -3,3 +3,6 @@
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk update
 apk add grafana
+mkdir -p /etc/grafana.d/datasources
+mkdir -p /etc/grafana.d/dashboards
+mkdir -p /etc/grafana.d/notifiers
