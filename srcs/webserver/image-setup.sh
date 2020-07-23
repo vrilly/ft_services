7a988f478610d63affeb05a640d87bdce4f96711
Tom Jans <tjans@student.codam.nl>
2020-07-23 16:13:14 +0200

    Configure webserver + add monitoring

--- srcs/webserver/image-setup.sh
+++ srcs/webserver/image-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-apk add nginx openssh
+apk add nginx openssh openssl
+rm /etc/nginx/conf.d/default.conf
 mkdir -p /run/nginx
+mkdir -p /usr/share/ssl/certs
+cd /usr/share/ssl/certs
+openssl req -x509 -sha256 -nodes -days 365 -subj "/CN=HTTP" -newkey rsa:2048 -keyout nginx.key -out nginx.pem
