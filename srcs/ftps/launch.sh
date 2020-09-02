e6f1964c5355adcfff2508f855e22addd8a699e0
Tom Jans <tjans@fluffderg.me>
2020-09-02 14:38:25 +0200

    Fix FTP

--- srcs/ftps/launch.sh
+++ srcs/ftps/launch.sh
@@ -4,6 +4,4 @@ sleep 5
 CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 BEARER=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 STAT_URL=https://kubernetes/api/v1/namespaces/default/services/ftps
-IP=$(curl --cacert "$CA_CERT" -H "Authorization: Bearer $BEARER" "$STAT_URL" | jq -r '.status.loadBalancer.ingress[0].ip')
-echo "pasv_address=$IP" >> /etc/vsftpd.conf
 /usr/sbin/vsftpd
