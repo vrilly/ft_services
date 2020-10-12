7c03ac85fbf82dbd86f55069db0cfd7b005befd5
Tom Jans <tjans@fluffderg.me>
2020-10-12 18:42:46 +0200

    did some wp stuff idk

--- srcs/wordpress/firsttime.sh
+++ srcs/wordpress/firsttime.sh
@@ -5,6 +5,7 @@ then
 	mkdir -p /var/www/wordpress
 	cd /var/www/wordpress
 	wp core download
+	chmod -R 0777 wp-content
 	wp config create \
 		--dbname=wordpress \
 		--dbuser=wordpress \
@@ -15,6 +16,9 @@ then
 		--admin_password="$WP_ADMIN_SECRET" \
 		--url="http://192.168.99.122:5050" \
 		--admin_email="postmaster@dum.my"
+	wp user create demo1 demo1@dum.my --role=subscriber --display_name="Demo Subscriber" --user_pass=demo1
+	wp user create demo2 demo2@dum.my --role=author --display_name="Demo Author" --user_pass=demo2
+	wp user create demo3 demo3@dum.my --role=editor --display_name="Demo Editor" --user_pass=demo3
 fi
 
 /usr/bin/supervisord
--- srcs/wordpress/image-setup.sh
+++ srcs/wordpress/image-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-apk add nginx php7-fpm php7-phar php7-json curl php7-curl php7-openssl php7-pdo_mysql php7-mysqli php7-iconv mysql-client supervisor
+apk add nginx php7-fpm php7-phar php7-json curl php7-curl \
+	php7-openssl php7-pdo_mysql php7-mysqli php7-iconv \
+	php7-gd php7-zip php7-imagick php7-mbstring php7-fileinfo \
+	php7-exif php7-dom mysql-client supervisor
 rm /etc/nginx/conf.d/default.conf
 mkdir -p /run/nginx
 curl -o /usr/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
